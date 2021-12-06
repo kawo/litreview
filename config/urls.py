f973395d0df4ff72a137406ca2b16c92bff631c4
Kévin Audebrand <kevin.audebrand@gmail.com>
2021-12-06 18:43:33 +0100

    base template

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", include("litreview.users.urls")),
+    path(
+        "",
+        TemplateView.as_view(template_name="pages/home.html"),
+        name="home",
+    ),
     path("admin/", admin.site.urls),
 ]
